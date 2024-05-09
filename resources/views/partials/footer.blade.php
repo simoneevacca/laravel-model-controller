1ad5fd2838d58be74ca8fd4ae77ce5ec890834af
simoneevacca <v.simone.vacca@gmail.com>
2024-05-09 15:41:08 +0200

    add some style to the cards with sass

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="bg-dark p-5">
     <div class="container text-white">
-        <div class="row row-col-3">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
             <div class="col">
-                <img src="https://picsum.photos/300/300" alt="">
+                <img src="https://picsum.photos/250/250" alt="">
             </div>
             <div class="col">
                 <h4>Lorem.</h4>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,19 @@
           
 
     <div class="container">
-        <div class="row row-cols-4">
-
+        <h1>Movies List</h1>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
             @forelse ($movies as $movie)
             <div class="col">
                 <div class="card">
-                        <div>Titolo: {{$movie->title}}</div>
-                        <div>Titolo originale: {{$movie->original_title}}</div>
-                        <div>Nazionalità: {{$movie->nationality}}</div>
-                        <div>Data: {{$movie->date}}</div>
-                        <div>Voto: {{$movie->vote}}</div>
-
+             
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Titolo:</strong> {{$movie->title}}</li>
+                            <li class="list-group-item"><strong>Titolo originale:</strong> {{$movie->original_title}}</li>
+                            <li class="list-group-item"><strong>Nazionalità:</strong> {{$movie->nationality}}</li>
+                            <li class="list-group-item"><strong>Data:</strong> {{$movie->date}}</li>
+                            <li class="list-group-item"><strong>Voto:</strong> {{$movie->vote}}</li>
+                          </ul>
 
                         
                     </div>
