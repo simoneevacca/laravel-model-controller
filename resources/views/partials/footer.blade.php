b23d5bbf72b21dd3d8c45bbbaa68621ee8d78f51
simoneevacca <v.simone.vacca@gmail.com>
2024-05-09 15:24:29 +0200

    style header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,32 @@
-<footer>
-    <div class="container">
-        footer
+<footer class="bg-dark p-5">
+    <div class="container text-white">
+        <div class="row row-col-3">
+            <div class="col">
+                <img src="https://picsum.photos/300/300" alt="">
+            </div>
+            <div class="col">
+                <h4>Lorem.</h4>
+                <ul class="list-group">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                  </ul>
+            </div>
+            
+            <div class="col">
+                <h4>Lorem.</h4>
+
+                <ul class="list-group">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                  </ul>
+            </div>
+        </div>
+
     </div>
 </footer>
