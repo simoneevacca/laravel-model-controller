1f033824dd76b97d2822fe0b8b944b001b02318a
simoneevacca <v.simone.vacca@gmail.com>
2024-05-09 13:45:19 +0200

    dd of movies db

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
+
 
 class PageController extends Controller
 {
     public function index() {
 
+
+        $movie = Movie::all();
+        dd($movie);
         return view('welcome');
     }
 }
